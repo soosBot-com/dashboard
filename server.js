e7702001557e4bd0032290709c12dab1d07cf89f
soos <62181284+sooswastaken@users.noreply.github.com>
2021-07-13 08:43:54 -0400

    Added css

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const config = require('./config.json');
 const fetch = require('node-fetch');
 const FormData = require('form-data');
+const cors = require('cors');
 
 app.get('/dashboard/login/callback', async (req, resp) => {
-  const accessCode = req.query.code;
-  console.log(accessCode);
+  const accessCode = req.query.code;;
   if (!accessCode) {
-      console.log("ERROR")
-      return resp.send("ERROR")
+      return resp.send("No code was provided.")
   } else {
+      console.log(accessCode)
       const data = new FormData();
       data.append('client_id', config.oauth2.client_id);
       data.append('client_secret', config.oauth2.secret);
@@ -21,25 +21,26 @@ app.get('/dashboard/login/callback', async (req, resp) => {
   
       const json = await (await fetch('https://discord.com/api/oauth2/token', {method: 'POST', body: data})).json();
       console.log(json.access_token)
-
-      resp.redirect(`http://192.168.1.224:3000/login/callback/?bearer_token=${json.access_token}`);
+      resp.redirect(`http://localhost:3000/login/callback/?token=${json.access_token}`);
   }
 });
-app.get('/dashboard/verify', async (req, resp) => {
+app.get('/dashboard/verify', cors(), async (req, resp) => {
   const fetch_user = await fetch(`https://discord.com/api/users/@me`, {headers: { Authorization: `Bearer ${req.query.token}` } });
   const user = await fetch_user.json();
   if (user.username) {
+    console.log(`${user.username} has logged in.`);
     return resp.send(true)
   } else{
     return resp.send(false)
   }
 });
 
-app.get('/dashboard/id', async (req, resp) => {
-  console.log("Bruh fucking work")
-  const fetch_user = await fetch(`https://discord.com/api/users/@me`, {headers: { Authorization: `Bearer ${req.query.bearer_token}` } });
+app.get('/dashboard/user', cors(), async (req, resp) => {
+  const fetch_user = await fetch(`https://discord.com/api/users/@me`, {headers: { Authorization: `Bearer ${req.query.token}` } });
   const user = await fetch_user.json();
+  console.log(user)
   return resp.send(user)
+
 })
 
   
